32c3fffc695035770d2f7310df576920c160d6c5
KOH HONG DA <hkoh006@KOHs-MacBook-Pro.local>
2017-08-21 11:57:11 +0800

    210817 indexing get_state()

--- Main.py
+++ Main.py
@@ -1,9 +1,23 @@
 from uiautomator import Device
+import xml.etree.ElementTree as ET
 
-d = Device('emulator-5554')
-print d.info
-d.screen.on()
-d.press.home()
-# d(text="Clock").click()
-xml = d.dump()
-print xml
+device_name = 'emulator-5554'
+
+
+def get_state():
+    xml = d.dump()
+    bit_rep = ''
+    root = ET.fromstring(xml.encode('utf-8'))
+    print root
+    for element in root.iter('node'):
+        bit_rep += element.get('index')
+    return bit_rep
+
+#text, resource_id, xlass,
+
+if __name__ == '__main__':
+    d = Device(device_name)
+    d.screen.on()
+    # d.press.home()
+    # d(text="Clock").click()
+    print get_state()
